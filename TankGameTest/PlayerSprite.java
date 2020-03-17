f9ea0e58248b3521b318cd85b940ae7004e478df
HunterMetcalf <60361746+HunterMetcalf@users.noreply.github.com>
2020-03-17 14:20:07 -0700

    Add files via upload

--- TankGameTest/PlayerSprite.java
+++ TankGameTest/PlayerSprite.java
@@ -1,8 +1,14 @@
 package com.cs240.tankgame;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 
 public class PlayerSprite extends Enemy{
+
+    int currentRotation;
+
+    Matrix matrix = new Matrix();
 
     public PlayerSprite(TankMap map, int col, int row, int facing, Bitmap bmp, int width, int height) {
         this.map = map;
@@ -13,7 +19,59 @@ public class PlayerSprite extends Enemy{
         this.health = 1;
         tookTurn = true;
         isBullet = false;
-
+        currentRotation = 0;
+        matrix.setRotate(90);
         this.image = bmp.createScaledBitmap(bmp, width, height, true);
     }
+
+    public void doTurn(){
+        tookTurn = true;
+    }
+
+    //Next two methods should be called externally
+    public void doMove(int facing){
+
+        moveFacing = facing;
+        if(map.move(this)){
+           map.doTurns();
+        } else System.out.println("Illegal move attempted"); //Handle this some other way later;
+        while(currentRotation != moveFacing) {
+            image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), matrix, true);
+            if (currentRotation != 3) currentRotation++;
+            else currentRotation = 0;
+        }
+    }
+
+    public void doShoot(int facing){
+
+        fireFacing = facing;
+        if(map.shoot(this, 2, 2)){
+            map.doTurns();
+        } else System.out.println("Illegal move attempted"); //Handle this some other way later;
+        while(currentRotation != fireFacing) {
+            image = Bitmap.createBitmap(image, 0, 0, image.getWidth(), image.getHeight(), matrix, true);
+            if (currentRotation != 3) currentRotation++;
+            else currentRotation = 0;
+        }
+    }
+
+    public void doHit(int damage){
+        health -= damage;
+        if(health <= 0) doDie();
+    }
+
+    public void doDie(){
+        map.setAtLoc(null, col, row);
+        //Handle losing the game
+    }
+
+    public Bitmap drawEnemy(Canvas canvas){
+        return image;
+        //canvas.drawBitmap(image, screenWidth/row, screenHeight/col, null);
+    }
+
+    public String toString() {
+        return icon + "";
+    }
+
 }
