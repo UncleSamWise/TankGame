6c4c2e31dda50f3ba4cd687a46b9a4e75cd033e2
UncleSamWise <60195312+UncleSamWise@users.noreply.github.com>
2020-03-19 15:33:21 -0700

    Add files via upload

--- TankGame/app/src/main/java/com/cs240/tankgame/MainActivity.java
+++ TankGame/app/src/main/java/com/cs240/tankgame/MainActivity.java
@@ -1,22 +1,33 @@
 package com.cs240.tankgame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import androidx.core.view.GestureDetectorCompat;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-    private GameView gameView;
 
-    // This is the gesture detector compat instance.
-    //private GestureDetectorCompat gestureDetectorCompat = null;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openGameWindow();
+            }
+
+        });
+    }
+    public void openGameWindow(){
         setContentView(new GameView(this));
     }
 }
